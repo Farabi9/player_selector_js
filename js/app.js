94a1c95dac69fc9f5fe1525e8b6274bc2a1a9d4e
rifat9 <ifatkhan633@gmail.com>
2023-01-23 03:52:13 +0600

    player name found

--- js/app.js
+++ js/app.js
@@ -7,26 +7,78 @@ function playerCost(id) {
     playerExpense.innerText = totalPlayerExpense;
     return totalPlayerExpense;
 }
-
+// <!-- class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"> -->
 document.getElementById('calculate').addEventListener('click', function () {
     playerCost('player-cost')
 })
+
+function managerCoach(id) {
+    const input = document.getElementById(id).value;
+    const inputValue = parseInt(input)
+    return inputValue;
+}
 
 document.getElementById('total-cost').addEventListener('click', function () {
 
 
     const totalPlayer = playerCost('player-cost')
 
-    const managerInput = document.getElementById('manager').value;
-    const managerInputValue = parseInt(managerInput);
+    const manager = managerCoach('manager')
 
+    const coach = managerCoach('coach')
 
-    const coachInput = document.getElementById('coach').value;
-    const coachInputValue = parseInt(coachInput);
-
-    const totalExpense = totalPlayer + managerInputValue + coachInputValue;
+    const totalExpense = totalPlayer + manager + coach;
 
     const costValue = document.getElementById('cost');
     costValue.innerText = totalExpense
 
 })
+
+function getName(id) {
+    const selectId = document.getElementById(id)
+    const name = selectId.getElementsByTagName('h5')
+    const playerName = name[0].innerText;
+    const nameField = document.getElementById('name');
+    nameField.innerText = playerName;
+    return playerName;
+}
+
+function getGoal(goalId) {
+    const id = document.getElementById(goalId);
+    const goal = id.getElementsByTagName('p')[0].innerText;
+    const goalField = document.getElementById('goal');
+    goalField.innerText = goal;
+    return goal;
+}
+
+
+document.getElementById('select1').addEventListener('click', function () {
+    getName('neymer')
+    getGoal('neymer')
+})
+document.getElementById('select2').addEventListener('click', function () {
+    getName('richarlison')
+
+})
+document.getElementById('select3').addEventListener('click', function () {
+    getName('alison')
+})
+document.getElementById('select4').addEventListener('click', function () {
+    getName('antony')
+})
+document.getElementById('select5').addEventListener('click', function () {
+    getName('jesus')
+})
+document.getElementById('select6').addEventListener('click', function () {
+    getName('pedro')
+})
+document.getElementById('select7').addEventListener('click', function () {
+    getName('rapinho')
+})
+document.getElementById('select8').addEventListener('click', function () {
+    getName('silva')
+})
+document.getElementById('select9').addEventListener('click', function () {
+    getName('vinius')
+})
+
